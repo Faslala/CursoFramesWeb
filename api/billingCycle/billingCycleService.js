e37b44e1c5ab294923fe4507992a04a10a89623b
Faslala Assis <faslala1@gmail.com>
2023-03-21 11:39:27 -0300

    commit do backend mas nao funcionando 100%. Precisa verificar as rotas

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,7 +1,28 @@
+//const { locals } = require('../../config/server')
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res;locals.bundle
+
+    if (bundle.errors) {
+        var erros = parseErrors(bundle.errors)
+        res.status(500).json({ errors })
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeRestfulError) {
+    const errors = []
+    _.forIn(nodeRestfulError, error => errors.push(error.message))
+    return errors
+}
 
 BillingCycle.route('count', function(req, res, next) {
     BillingCycle.count(function(error, value) {
